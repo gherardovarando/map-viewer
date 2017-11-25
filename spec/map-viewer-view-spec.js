75528e2f97a8a35b5208c0718399e5514639bc54
gherardovarando <gherardo.varando@gmail.com>
2017-11-25 09:24:43 +0100

    initial

---
+++ spec/map-viewer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MapViewerView from '../lib/map-viewer-view';
+
+describe('MapViewerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
